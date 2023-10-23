30853694c817aa9eb6a0864efb2b7043662a895b
Tony Collins <123tonycollins@gmail.com>
2023-10-23 20:32:28 +0100

    2 player finished

--- OSAMDXS.py
+++ OSAMDXS.py
@@ -11,27 +11,35 @@ class Game:
     def play(self):
         winCode =4
         count=0
+        self.board.printBoard()
+        
         while(winCode==4):
-            pos=self.move()
+           
             if self.count % 2 ==0: 
                 OorX ="X"
             else :
                 OorX="O"
-            
+            print(" {0}'s Move".format(OorX))
+            pos=self.move()
             self.board.update(OorX,pos)
+            
             self.board.printBoard()
             winCode=self.board.checkwin(OorX)
             self.count=self.count+1
-            
-            
-            
+        if winCode == 1 :
+            print("Xs Win")
+          
+        if winCode == 2 :
+            print("Os Win")
+        if winCode == 3 :
+            print("Draw")
            
     def move(self):
         while True:
             
-            print("Please enter a valid position to move t0")
+            print("Please enter a valid position to move to")
             pos = int(input());
-            if pos>0 and pos<10 and not(self.board.getValuePos(pos)=="X" or self.board.getValuePos(pos)=="X") :
+            if pos>0 and pos<10 and not(self.board.getValuePos(pos)=="X" or self.board.getValuePos(pos)=="O") :
                 break
         return pos-1
             
@@ -56,15 +64,20 @@ class Board:
     def getValuePos(self,p):
         return self.board[p]
     def checkwin(self,OorX):
+        win_code=4
+        draw_count =0
         if OorX=="X":
            winCode = 1
         else:
             winCode =2
         for x in range(0,8):
-            if self.board[self.winnerlines[x][0]]==OorX and self.board[self.winnerlines[x][1]]==OorX and self.board[self.winnerlines[x][1]]==OorX:
+            if self.board[self.winnerlines[x][0]]==OorX and self.board[self.winnerlines[x][1]]==OorX and self.board[self.winnerlines[x][2]]==OorX:
                 return winCode
-           
-        return 4
+            if self.board[x]==" " :
+                draw_count=draw_count +1
+        if draw_count ==9:
+            winCode = 3
+        return win_code
             
             
            
